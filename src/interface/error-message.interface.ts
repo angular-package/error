40407503893484874e588b8b5b42c6e40a5fc3ab
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-08-12 00:04:20 +0200

    refactor(ErrorMessage): add template property.

--- src/interface/error-message.interface.ts
+++ src/interface/error-message.interface.ts
@@ -1,13 +1,22 @@
 /**
  * The shape of an `object` for the error message that contains a possible solution to the described problem.
+ * @param fix A possible solution to the described problem of a `string` type.
+ * @param problem Description of validation problem of a string type.
+ * @param template An optional message template of a `string` type.
  */
 export interface ErrorMessage {
   /**
-   * Possible solution to the described problem of a `string` type.
+   * A possible solution to the described problem of a `string` type.
    */
   fix: string;
+
   /**
-   * Error problem of a `string` type.
+   * Description of validation problem of a `string` type.
    */
   problem: string;
+
+  /**
+   * An optional message template of a `string` type.
+   */
+  template?: string;
 }
