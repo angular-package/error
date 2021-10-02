997e0e42b398431b101578f7dea9bb822e284fa8
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-10-02 23:36:52 +0200

    refactor(MessageBuilder): instead of `is` object use functions, use short if  as expression, change `set` prefix to `replace` of methods, update jsdoc. Still experimental.

--- src/message-builder/src/message-builder.class.ts
+++ src/message-builder/src/message-builder.class.ts
@@ -1,12 +1,20 @@
 // @angular-package/type
-import { is, guard, ResultCallback } from '@angular-package/type';
+import {
+  ResultCallback,
+  guardString,
+  isDefined,
+  isString,
+} from '@angular-package/type';
 // Class.
 import { MessageBuilderTemplate } from './message-builder-template.class';
 /**
- * Message builder for error message of a string type.
- * @version Experimental This `object` is an experimental version of the message builder.
+ * Message builder to build `class`, `function` and `method` of a `string` type.
+ * @version experimental
  */
 export class MessageBuilder {
+  /**
+   * An object with regexp to replace.
+   */
   #regExp = {
     class: /\[class\]/i,
     function: /\[function\]/i,
@@ -15,46 +23,75 @@ export class MessageBuilder {
       name: /\[param.name\]/i,
       type: /\[param.type\]/i,
     },
-    return: /\[return\]/i
+    return: /\[return\]/i,
   };
 
+  /**
+   * 
+   */
   #template: string;
 
+  /**
+   * 
+   */
   get get(): string {
     return this.#template;
   }
 
+  /**
+   * 
+   * @param template 
+   */
   constructor(template: 'class' | 'function' | 'method') {
     this.#template = new MessageBuilderTemplate(template).get;
   }
 
-  public setClassName(name: string, callback?: ResultCallback): this {
-    if (guard.string(name, callback)) {
-      this.replace(this.#regExp.class, name);
-    }
+  /**
+   * 
+   * @param name 
+   * @param callback 
+   * @returns 
+   */
+  public replaceClassName(name: string, callback?: ResultCallback): this {
+    guardString(name, callback) && this.replace(this.#regExp.class, name);
     return this;
   }
 
-  public setFunctionName(name: string, callback?: ResultCallback): this {
-    if (guard.string(name, callback)) {
-      this.replace(this.#regExp.function, name);
-    }
+  /**
+   * 
+   * @param name 
+   * @param callback 
+   * @returns 
+   */
+  public replaceFunctionName(name: string, callback?: ResultCallback): this {
+    guardString(name, callback) && this.replace(this.#regExp.function, name);
     return this;
   }
 
-  public setMethodName(name: string, callback?: ResultCallback): this {
-    if (guard.string(name, callback)) {
-      this.replace(this.#regExp.method, name);
-    }
+  /**
+   * 
+   * @param name 
+   * @param callback 
+   * @returns 
+   */
+  public replaceMethodName(name: string, callback?: ResultCallback): this {
+    guardString(name, callback) && this.replace(this.#regExp.method, name);
     return this;
   }
 
-  public setParam(name: string, type: string = ''): this {
-    if (guard.string(name)) {
+  /**
+   * 
+   * @param name 
+   * @param type 
+   * @returns 
+   */
+  public replaceParam(name: string, type: string = ''): this {
+    if (guardString(name)) {
       const param = `${name}${type}`;
-      this
-        .replace(this.#regExp.param.name, name)
-        .replace(this.#regExp.param.type, type);
+      this.replace(this.#regExp.param.name, name).replace(
+        this.#regExp.param.type,
+        type
+      );
 
       if (type.length > 0) {
         this.replace(type, `: ${type}`);
@@ -63,20 +100,32 @@ export class MessageBuilder {
     return this;
   }
 
-  public setReturn(returns: string, callback?: ResultCallback): this {
-    if (guard.string(returns, callback)) {
-      this.replace(this.#regExp.return, returns.length > 0 ? `: ${returns}` : returns);
-    }
+  /**
+   * 
+   * @param returns 
+   * @param callback 
+   * @returns 
+   */
+  public replaceReturn(returns: string, callback?: ResultCallback): this {
+    guardString(returns, callback) &&
+      this.replace(
+        this.#regExp.return,
+        returns.length > 0 ? `: ${returns}` : returns
+      );
     return this;
   }
 
+  /**
+   * 
+   * @param searchValue 
+   * @param replaceValue 
+   * @returns 
+   */
   private replace(searchValue: string | RegExp, replaceValue: string): this {
-    if (is.defined(searchValue)) {
-      this.#template = this.#template.replace(
-        searchValue,
-        is.string(replaceValue) ? replaceValue : ''
-      );
-    }
+    isDefined(searchValue) && (this.#template = this.#template.replace(
+      searchValue,
+      isString(replaceValue) ? replaceValue : ''
+    ));
     return this;
   }
 }
