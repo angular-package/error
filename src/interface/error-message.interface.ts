8cc7528035bde6b68a1ff80738d7c06fe10f02a2
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-10-04 19:35:51 +0200

    refactor(ErrorMessage): add  an optional `value` property affected by the validation error, which must be parsed to `string`.

--- src/interface/error-message.interface.ts
+++ src/interface/error-message.interface.ts
@@ -1,8 +1,9 @@
 /**
  * The shape of an `object` for the error message that contains a possible solution to the described problem.
  * @param fix A possible solution to the described problem of a `string` type.
- * @param problem Description of validation problem of a string type.
+ * @param problem Description of validation problem of a `string` type.
  * @param template An optional message template of a `string` type.
+ * @param value An optional value affected by the validation error, which must be parsed to `string`.
  */
 export interface ErrorMessage {
   /**
@@ -11,12 +12,17 @@ export interface ErrorMessage {
   fix: string;
 
   /**
-   * Description of validation problem of a `string` type.
+   * Description of validation problem of string type.
    */
   problem: string;
 
   /**
-   * An optional message template of a `string` type.
+   * An optional message template of a string type.
    */
   template?: string;
+
+  /**
+   * An optional value affected by the validation error, which must be parsed to `string`.
+   */
+  value?: string;
 }
