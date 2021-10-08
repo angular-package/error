47a88cc14fdca778335304cb15e6b368bbaa8d05
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-10-08 21:44:48 +0200

    docs(ValueParser): update description.

--- src/type/value-parser.type.ts
+++ src/type/value-parser.type.ts
@@ -1,4 +1,4 @@
 /**
- * The function to convert the value of any type to `string`.
+ * The type of function to convert the value of any type to `string`.
  */
 export type ValueParser = (value: any) => string;
