60f07a340cfeeca997cf2b8541971f2a2e24b0dc
angularpackage <contact@angular-package.dev>
2021-12-18 23:46:03 +0100

    refactor(MessageBuilder): move to text package.

--- src/message-builder/index.ts
+++
@@ -1,3 +0,0 @@
-export { MessageBuilder } from './src/message-builder.class';
-export { MessageBuilderTemplate } from './src/message-builder-template.class';
-export { MessageFunctionBuilder } from './src/message-function-builder.class';
