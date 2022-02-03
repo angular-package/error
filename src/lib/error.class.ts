789083e5c79d6ee0f1f098bcc5a352a8dccf939b
angularpackage <contact@angular-package.dev>
2022-02-03 15:46:43 +0100

    feat(Error): add `Error` object extension for the `CommonError` class.

---
+++ src/lib/error.class.ts
@@ -0,0 +1,60 @@
+import { CommonError } from './common-error.class';
+/**
+ * The `Error` object is an extension for javascript `Error` which means it's thrown when a runtime error occurs but with additional
+ * identification and a solution to the described problem.
+ */
+export class Error<Id extends string> extends CommonError<Id> {
+  /**
+   * Error name of a `string` type, set to `Error` that is being thrown.
+   * @angularpackage
+   */
+  public get name(): string {
+    return 'Error';
+  }
+
+  /**
+   * The `get` accessor, with the help of `toStringTag`, changes the default tag to `'Error'` for an instance of `Error`. It can be read
+   * by the `typeOf()` function of `@angular-package/type`.
+   * @returns The return value is the word 'Error` of a `string`.
+   * @angularpackage
+   */
+  public get [Symbol.toStringTag](): string {
+    return 'Error';
+  }
+
+  /**
+   * Checks whether the value of any type is an instance of `Error` of any or the given identification.
+   * @param value The value of any type to check against the `Error` instance.
+   * @param id Optional identification of generic type variable `Id` that the given `value` contains.
+   * @returns The return value is a `boolean` type indicating whether the given `value` is an instance of `Error` of any or the given `id`.
+   */
+   public static isError<Id extends string>(
+    value: any,
+    id?: Id
+  ): value is Error<Id> {
+    return typeof value === 'object' &&
+      value instanceof this &&
+      typeof id === 'string'
+      ? value.id === id
+      : true;
+  }
+
+  /**
+   * Creates the `Error` instance that represents an error with the described problem and its solution, optionally marked with an explicit
+   * identification.
+   * @param problem Description of the problem of a string type.
+   * @param fix A solution to the given `problem` of a string type.
+   * @param id Optional unique identification to the given `problem` of generic type variable `Id`.
+   * @param template A template of error message with the replaceable `{problem}`, `{fix}` and optional `{id}` words. By default, the value
+   * is picked from the static property `template`.
+   * @angularpackage
+   */
+  constructor(
+    problem: string,
+    fix: string,
+    id?: Id,
+    template = Error.template,
+  ) {
+    super(problem, fix, id, template);
+  }
+}
