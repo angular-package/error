f67f99dbc77140c4f4d891073d45baef273c9ebc
angularpackage <contact@angular-package.dev>
2022-02-03 20:16:54 +0100

    refactor(Error): add `define()` and `throw()` public static methods, add regions.

--- src/lib/error.class.ts
+++ src/lib/error.class.ts
@@ -4,6 +4,7 @@ import { CommonError } from './common-error.class';
  * identification and a solution to the described problem.
  */
 export class Error<Id extends string> extends CommonError<Id> {
+  //#region public instance accessors.
   /**
    * Error name of a `string` type, set to `Error` that is being thrown.
    * @angularpackage
@@ -21,6 +22,28 @@ export class Error<Id extends string> extends CommonError<Id> {
   public get [Symbol.toStringTag](): string {
     return 'Error';
   }
+  //#endregion public instance accessors.
+
+  //#region public static methods.
+  /**
+   * Defines the `RangeError` instance with the given required `problem`, `fix` and optional `id` and `template`.
+   * @param problem Description of the problem of a `string` type.
+   * @param fix A solution to the given `problem` of a `string` type.
+   * @param id Optional unique identification to the given `problem` of generic type variable `Id`.
+   * @param template A template of error message with the replaceable `{problem}`, `{fix}` and optional `{id}` words. By default, the value
+   * is picked from the static property `template`.
+   * @returns The return value is a new instance of the `RangeError` with the given required `problem`, `fix` and optional `id` and
+   * `template`.
+   * @angularpackage
+   */
+  public static define<Id extends string = ''>(
+    problem: string,
+    fix: string,
+    id?: Id,
+    template = Error.template
+  ): Error<Id> {
+    return new this(problem, fix, id, template);
+  }
 
   /**
    * Checks whether the value of any type is an instance of `Error` of any or the given identification.
@@ -28,7 +51,7 @@ export class Error<Id extends string> extends CommonError<Id> {
    * @param id Optional identification of generic type variable `Id` that the given `value` contains.
    * @returns The return value is a `boolean` type indicating whether the given `value` is an instance of `Error` of any or the given `id`.
    */
-   public static isError<Id extends string>(
+  public static isError<Id extends string>(
     value: any,
     id?: Id
   ): value is Error<Id> {
@@ -39,6 +62,27 @@ export class Error<Id extends string> extends CommonError<Id> {
       : true;
   }
 
+  /**
+   * Throws the `RangeError` with the given required `problem`, `fix` and optional `id` and `template`.
+   * @param problem Description of the problem of a `string` type.
+   * @param fix A solution to the given `problem` of a `string` type.
+   * @param id Optional unique identification to the given `problem` of generic type variable `Id`.
+   * @param template A template of error message with the replaceable `{problem}`, `{fix}` and optional `{id}` words. By default, the value
+   * is picked from the static property `template`.
+   * @angularpackage
+   */
+  public static throw(
+    problem: string,
+    fix: string,
+    id?: string,
+    template?: string
+  ): void {
+    throw this.define(problem, fix, id, template);
+  }
+
+  //#endregion public static methods.
+
+  //#region constructor.
   /**
    * Creates the `Error` instance that represents an error with the described problem and its solution, optionally marked with an explicit
    * identification.
@@ -53,8 +97,9 @@ export class Error<Id extends string> extends CommonError<Id> {
     problem: string,
     fix: string,
     id?: Id,
-    template = Error.template,
+    template = Error.template
   ) {
     super(problem, fix, id, template);
   }
+  //#endregion constructor.
 }
