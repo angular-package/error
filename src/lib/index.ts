e83cc30b73a67b51a70a5099f08396820f45d3d8
angularpackage <contact@angular-package.dev>
2022-02-04 00:09:21 +0100

    chore(index): remove unnecessary objects `EvalError` `InternalError` `ReferenceError` `SyntaxError` `URIError`.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,11 +1,6 @@
 export { CommonError } from './common-error.class';
 export { Error } from './error.class';
 export { Errors } from './errors.class';
-export { EvalError } from './eval-error.class';
-export { InternalError } from './internal-error.class';
 export { RangeError } from './range-error.class';
-export { ReferenceError } from './reference-error.class';
-export { SyntaxError } from './syntax-error.class';
 export { TypeError } from './type-error.class';
-export { URIError } from './uri-error.class';
 export { ValidationError } from './validation-error.class';
