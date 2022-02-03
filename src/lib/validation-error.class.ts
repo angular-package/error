723f1e15350f05cfcc48d91ef0c7495a8da37a06
angularpackage <contact@angular-package.dev>
2022-02-04 00:03:57 +0100

    refactor(ValidationError): reorder class members, change the static public `define()` `isValidationError()` `throw()` methods to use parent methods by using super and updatej jsdoc.

--- src/lib/validation-error.class.ts
+++ src/lib/validation-error.class.ts
@@ -1,49 +1,9 @@
 import { CommonError } from './common-error.class';
 /**
- * The `ValidationError` object is thrown when an operation could not be performed despite proper type and proper expected value, along with
+ * The `ValidationError` object is thrown when an operation could not be performed despite proper type(but not exclusively), along with
  * additional identification and a solution to the described problem.
  */
 export class ValidationError<Id extends string = ''> extends CommonError {
-  //#region public static methods.
-  /**
-   * Defines the `ValidationError` instance with the given required `problem`, `fix` and optional `id` and `template`.
-   * @param problem Description of the problem of a `string` type.
-   * @param fix A solution to the given `problem` of a `string` type.
-   * @param id Optional unique identification to the given `problem` of generic type variable `Id`.
-   * @param template A template of error message with the replaceable `{problem}`, `{fix}` and optional `{id}` words. By default, the value
-   * is picked from the static property `template`.
-   * @returns The return value is a new instance of the `ValidationError` with the given required `problem`, `fix` and optional `id` and
-   * `template`.
-   * @angularpackage
-   */
-  public static define<Id extends string = ''>(
-    problem: string,
-    fix: string,
-    id?: Id,
-    template = ValidationError.template
-  ): ValidationError<Id> {
-    return new this(problem, fix, id, template);
-  }
-
-  /**
-   * Throws the `ValidationError` with the given required `problem`, `fix` and optional `id` and `template`.
-   * @param problem Description of the problem of a `string` type.
-   * @param fix A solution to the given `problem` of a `string` type.
-   * @param id Optional unique identification to the given `problem` of generic type variable `Id`.
-   * @param template A template of error message with the replaceable `{problem}`, `{fix}` and optional `{id}` words. By default, the value
-   * is picked from the static property `template`.
-   * @angularpackage
-   */
-  public static throw(
-    problem: string,
-    fix: string,
-    id?: string,
-    template?: string
-  ): void {
-    throw this.define(problem, fix, id, template);
-  }
-  //#endregion public static methods.
-
   //#region public instance accessors.
   /**
    * Error name of a `string` type, set to `ValidationError` that is being thrown.
@@ -66,6 +26,26 @@ export class ValidationError<Id extends string = ''> extends CommonError {
 
   //#region public static methods.
   /**
+   * Defines the `ValidationError` instance with the given required `problem`, `fix` and optional `id` and `template`.
+   * @param problem Description of the problem of a `string` type.
+   * @param fix A solution to the given `problem` of a `string` type.
+   * @param id Optional unique identification to the given `problem` of generic type variable `Id`.
+   * @param template A template of error message with the replaceable `{problem}`, `{fix}` and optional `{id}` words. By default, the value
+   * is picked from the static property `template`.
+   * @returns The return value is a new instance of the `ValidationError` with the given required `problem`, `fix` and optional `id` and
+   * `template`.
+   * @angularpackage
+   */
+  public static define<Id extends string = ''>(
+    problem: string,
+    fix: string,
+    id?: Id,
+    template = ValidationError.template
+  ): ValidationError<Id> {
+    return super.define(problem, fix, id, template, this);
+  }
+
+  /**
    * Checks whether the value of any type is an instance of `ValidationError`.
    * @param value The value of any type to check against the `ValidationError` instance.
    * @returns The return value is a `boolean` type indicating whether the given `value` is an instance of `ValidationError`.
@@ -74,11 +54,25 @@ export class ValidationError<Id extends string = ''> extends CommonError {
     value: any,
     id?: Id
   ): value is ValidationError<Id> {
-    return typeof value === 'object' &&
-      value instanceof this &&
-      typeof id === 'number'
-      ? value.id === id
-      : true;
+    return super.isError(value, id);
+  }
+
+  /**
+   * Throws the `ValidationError` with the given required `problem`, `fix` and optional `id` and `template`.
+   * @param problem Description of the problem of a `string` type.
+   * @param fix A solution to the given `problem` of a `string` type.
+   * @param id Optional unique identification to the given `problem` of generic type variable `Id`.
+   * @param template A template of error message with the replaceable `{problem}`, `{fix}` and optional `{id}` words. By default, the value
+   * is picked from the static property `template`.
+   * @angularpackage
+   */
+  public static throw(
+    problem: string,
+    fix: string,
+    id?: string,
+    template?: string
+  ): void {
+    super.throw(problem, fix, id, template);
   }
   //#endregion public static methods.
 
