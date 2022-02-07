61894b78d175ea9bf735f3378a8c8fa49b81f9ff
angularpackage <sciborrudnicki@wvvw.dev>
2022-02-07 16:07:28 +0100

    refactor(Error): remove default value of the generic type variable `Id` in the `define()` static method. Update jsdoc.

--- src/lib/error.class.ts
+++ src/lib/error.class.ts
@@ -1,12 +1,13 @@
 import { CommonError } from './common-error.class';
 /**
+ * The `Error` class is an extension of the` CommonError` class
  * The `Error` object is thrown when a runtime error occurs, but with additional identification and a solution to the described problem.
  */
 export class Error<Id extends string> extends CommonError<Id> {
   //#region public instance accessors.
   /**
    * Error name of a `string` type, set to `Error` that is being thrown.
-   * @returns The return value is the error instance name of string type.
+   * @returns The return value is the error instance name of `string` type.
    * @angularpackage
    */
   public get name(): string {
@@ -16,7 +17,7 @@ export class Error<Id extends string> extends CommonError<Id> {
   /**
    * The `get` accessor, with the help of `toStringTag`, changes the default tag to `'Error'` for an instance of `Error`. It can be read
    * by the `typeOf()` function of `@angular-package/type`.
-   * @returns The return value is the word 'Error` of a `string`.
+   * @returns The return value is the word `Error` of a `string`.
    * @angularpackage
    */
   public get [Symbol.toStringTag](): string {
@@ -26,16 +27,17 @@ export class Error<Id extends string> extends CommonError<Id> {
 
   //#region public static methods.
   /**
-   * Defines the `Error` instance with the given required `problem`, `fix` and optional `id` and `template`.
+   * Defines the `Error` instance with the message built of the given required `problem`, `fix` and optional `id` on the `template`.
    * @param problem Description of the problem of a `string` type.
    * @param fix A solution to the given `problem` of a `string` type.
    * @param id Optional unique identification to the given `problem` of generic type variable `Id`.
-   * @param template A template of error message with the replaceable `{problem}`, `{fix}` and optional `{id}` tags. By default, the value
-   * is picked from the static property `template`.
-   * @returns The return value is a new instance of the `Error` with the given required `problem`, `fix` and optional `id` and `template`.
+   * @param template A template of error message with the replaceable `{problem}`, `{fix}` and optional `{id}`, `{max}`, `{min}` and
+   * `{type}` tags. By default, the value is picked from the static property `template`.
+   * @returns The return value is a new instance of the `Error` with the message built from the given required `problem`, `fix` and optional
+   * `id` on the `template`.
    * @angularpackage
    */
-  public static define<Id extends string = ''>(
+  public static define<Id extends string>(
     problem: string,
     fix: string,
     id?: Id,
@@ -61,8 +63,8 @@ export class Error<Id extends string> extends CommonError<Id> {
 
   //#region constructor.
   /**
-   * Creates the `Error` instance that represents an error with the described problem and its solution, optionally marked with an explicit
-   * identification.
+   * Creates the `Error` instance that represents an error with the message built of the described `problem` and its solution, optionally
+   * marked with an explicit identification on the given or stored `template`.
    * @param problem Description of the problem of a string type.
    * @param fix A solution to the given `problem` of a string type.
    * @param id Optional unique identification to the given `problem` of generic type variable `Id`.
