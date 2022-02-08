dad8252ff8b19a6f9e6e2f22d01850d62c9c451d
angularpackage <sciborrudnicki@wvvw.dev>
2022-02-09 00:41:28 +0100

    refactor(ValidationError): remove static `throw()` method, change the static `define()` method to not use `super.define()`. Update jsdoc.

--- src/lib/validation-error.class.ts
+++ src/lib/validation-error.class.ts
@@ -1,12 +1,14 @@
 import { CommonError } from './common-error.class';
 /**
- * The `ValidationError` object is thrown when an operation could not be performed despite proper type(but not exclusively), along with
- * additional identification and a solution to the described problem.
+ * The `ValidationError` object is an extension of the `CommonError` class and is thrown when an operation could not be performed despite
+ * proper type(but not exclusively) with the message built from the described problem and its solution, along with additional identification
+ * on the given or stored template.
  */
 export class ValidationError<Id extends string = ''> extends CommonError {
   //#region public instance accessors.
   /**
-   * Error name of a `string` type, set to `ValidationError` that is being thrown.
+   * The `get` accessor obtains error name of a `string` type, set to 'ValidationError' that is being thrown.
+   * @returns The return value is the error instance name of `string` type.
    * @angularpackage
    */
   public get name(): string {
@@ -26,29 +28,33 @@ export class ValidationError<Id extends string = ''> extends CommonError {
 
   //#region public static methods.
   /**
-   * Defines the `ValidationError` instance with the given required `problem`, `fix` and optional `id` and `template`.
+   * Defines the `ValidationError` instance with the message built of the given required `problem`, `fix`, and optional `id` on the supplied
+   * or stored `template`.
    * @param problem Description of the problem of a `string` type.
    * @param fix A solution to the given `problem` of a `string` type.
    * @param id Optional unique identification to the given `problem` of generic type variable `Id`.
-   * @param template A template of error message with the replaceable `{problem}`, `{fix}` and optional `{id}` words. By default, the value
-   * is picked from the static property `template`.
-   * @returns The return value is a new instance of the `ValidationError` with the given required `problem`, `fix` and optional `id` and
-   * `template`.
+   * @param template A template of error message with the replaceable `{problem}`, `{fix}` and optional `{id}`, `{min}`, `{max}` and
+   * `{type}` tags. By default, the value is picked from the static property `ValidationError.template`.
+   * @returns The return value is a new instance of the `ValidationError` with the message built from the given required `problem`, `fix`
+   * and optional `id` on the given or stored `template`.
    * @angularpackage
    */
-  public static define<Id extends string = ''>(
+  public static define<Id extends string>(
     problem: string,
     fix: string,
     id?: Id,
     template = ValidationError.template
   ): ValidationError<Id> {
-    return super.define(problem, fix, id, template, this);
+    return new this(problem, fix, id, template);
   }
 
   /**
-   * Checks whether the value of any type is an instance of `ValidationError`.
+   * Checks whether the value of any type is the `ValidationError` instance of any or the given identification.
    * @param value The value of any type to check against the `ValidationError` instance.
-   * @returns The return value is a `boolean` type indicating whether the given `value` is an instance of `ValidationError`.
+   * @param id Optional unique identification of generic type variable `Id` to check whether the given `value` contains.
+   * @returns The return value is a `boolean` type indicating whether the given `value` is an instance of `ValidationError` of any or the
+   * supplied optional id properties.
+   * @angularpackage
    */
   public static isValidationError<Id extends string>(
     value: any,
@@ -56,41 +62,24 @@ export class ValidationError<Id extends string = ''> extends CommonError {
   ): value is ValidationError<Id> {
     return super.isError(value, id);
   }
-
-  /**
-   * Throws the `ValidationError` with the given required `problem`, `fix` and optional `id` and `template`.
-   * @param problem Description of the problem of a `string` type.
-   * @param fix A solution to the given `problem` of a `string` type.
-   * @param id Optional unique identification to the given `problem` of generic type variable `Id`.
-   * @param template A template of error message with the replaceable `{problem}`, `{fix}` and optional `{id}` words. By default, the value
-   * is picked from the static property `template`.
-   * @angularpackage
-   */
-  public static throw(
-    problem: string,
-    fix: string,
-    id?: string,
-    template?: string
-  ): void {
-    super.throw(problem, fix, id, template);
-  }
   //#endregion public static methods.
 
   //#region constructor.
   /**
-   * Creates a `ValidationError` instance that represents validation error with the described problem and its solution, optionally marked
-   * with an explicit identification.
+   * Creates the `ValidationError` instance that represents validation error with the message built of the given described problem, its
+   * solution, and optional explicit identification, on the supplied or stored error message template.
    * @param problem Description of the validation problem of a `string` type.
    * @param fix A solution to the given validation issue of a `string` type.
    * @param id Optional unique identification to the given `problem` of generic type variable `Id`.
-   * @param template Optional template of error message with the replaceable `{problem}`, `{fix}` and optional `{id}` words. By default, the
-   * value is picked from the static property `template`.
+   * @param template Optional template of error message with the replaceable `{problem}`, `{fix}` and optional `{id}`, `{max}`, `{min}` and
+   * `{type}` tags. By default, the value is picked from the static property `template`.
    * @angularpackage
    */
   constructor(
     problem: string,
     fix: string,
     id?: Id,
+    // field?: string,
     template = ValidationError.template
   ) {
     super(problem, fix, id, template);
