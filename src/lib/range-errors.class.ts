d4ae81d4b6ec379768c84d21f85dbff4a9275e15
angularpackage <sciborrudnicki@wvvw.dev>
2022-02-13 01:51:44 +0100

    refactor(RangeErrors): add `toObject()` instance method and update jsdoc of the `set()` `constructor()`.

--- src/lib/range-errors.class.ts
+++ src/lib/range-errors.class.ts
@@ -1,14 +1,15 @@
 import { CommonErrors } from './common-errors.class';
 import { RangeError } from './range-error.class';
 /**
- * The `Errors` is an extension of the `CommonErrors` object that represents multiple identification numbers under which the errors of the
- * `RangeError` type are prepared to throw.
+ * The `RangeErrors` is an extension of the `CommonErrors` object that represents multiple identification numbers under which the errors of
+ * the `RangeError` type are prepared to throw.
  */
 export class RangeErrors<Id extends string> extends CommonErrors<Id> {
   //#region constructor.
   /**
    * Creates the `RangeErrors` instance of unique identification numbers under which the `RangeError` objects are stored.
-   * @param id The unique identification numbers of an `Array` of generic type variable `Id`.
+   * @param id A rest parameter of generic type variable `Id` indicates unique identification numbers under which the `RangeError` objects
+   * are stored.
    * @angularpackage
    */
   constructor(...id: Id[]) {
@@ -18,8 +19,8 @@ export class RangeErrors<Id extends string> extends CommonErrors<Id> {
 
   //#region instance public methods.
   /**
-   * Sets the range error with the message built from the given required `problem`, `fix`, `id` and optional `min`, `max` on the given or
-   * stored `template`.
+   * Sets the `RangeError` object with the message built from the given required `problem`, `fix`, `id` and optional `min`, `max` on the
+   * given or stored `template`.
    * @param problem Description of the problem of a string type.
    * @param fix A solution to the given `problem` of a string type.
    * @param id The unique identification to the given `problem` of generic type variable `Id`.
@@ -41,6 +42,15 @@ export class RangeErrors<Id extends string> extends CommonErrors<Id> {
     this.isAllowedId(id) &&
       this.errors.set(id, new RangeError(problem, fix, id, min, max, template));
     return this;
+  }
+
+  /**
+   * The method returns the JSON object of set errors, where the key is a unique identification.
+   * @returns The return value is an `object` of set errors.
+   * @angularpackage
+   */
+  public toObject(): { [Key in Id]: RangeError<Id> } {
+    return Object.fromEntries(this.errors.entries()) as any;
   }
   //#endregion instance public methods.
 }
